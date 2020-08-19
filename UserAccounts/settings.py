d9888a533a50c0ae232bb737d20cb7e58276cf01
Seelam Sanjay Ganesh <51526260+Sanju525@users.noreply.github.com>
2020-08-19 19:22:26 +0530

    Update settings.py

--- UserAccounts/settings.py
+++ UserAccounts/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '=1wgee^@4tcstz^jkdihr70w&8&$**oraa%cdxk@sy4qxjl7a&'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-
+    'myusersapp.herokuapp.com',
 ]
 
 # Application definition
@@ -143,10 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASS')
-EMAIL_HOST_USER = 'sanjayganesh.252@gmail.com'
-EMAIL_HOST_PASSWORD = 'eoztoghekegigubj'
+EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASS')
 
 
 
